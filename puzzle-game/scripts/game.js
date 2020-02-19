5125bdb6aeea45bcb1179e7eb79368c439a42e4d
ttgnu105 <lilasianboi12@yahoo.com>
2020-02-18 22:36:58 -0600

    adding game

--- puzzle-game/scripts/game.js
+++ puzzle-game/scripts/game.js
@@ -1,8 +1,16 @@
-let passcode = 500;
-let guess = prompt("Guess a number between 1- 1000");
-if(guess == passcode){
-	console.log("you win !!!!");
-}
-else{
-	console.log("you lost ;-;");
+let passcode = Math.floor(Math.random()*1000);
+let tries  = 10;
+while(tries > 0 && guess != passcode){
+	console.log("you have "+tries+"guesses left");
+	let guess = prompt("Guess a number:");
+	tries--;
+	if(guess == passcode){
+		console.log("you win !!!! you got it in "+tries+" guesses");
+	}
+	else if(tries == 0){
+		console.log("you lost ;-; the number was " +passcode);
+	}
+	else{
+		console.log("WRONG! Try again.")
+	}
 }
